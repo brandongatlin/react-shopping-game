3265f1b88f4d294096f327952bce992811460abd
brandongatlin <brandongatlin1981@me.com>
2019-07-24 20:43:02 -0500

    Updates

---
+++ precache-manifest.cc18ecde4f354f73b744d4a977d4219f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "88cdf420953582ccb945",
+    "url": "/react-shopping-game/static/css/main.1ee816af.chunk.css"
+  },
+  {
+    "revision": "88cdf420953582ccb945",
+    "url": "/react-shopping-game/static/js/main.88cdf420.chunk.js"
+  },
+  {
+    "revision": "c16fafc95c9d83345e4d",
+    "url": "/react-shopping-game/static/js/runtime~main.c16fafc9.js"
+  },
+  {
+    "revision": "856281628acf98f4f10c",
+    "url": "/react-shopping-game/static/js/2.85628162.chunk.js"
+  },
+  {
+    "revision": "72f406b837c3f63a460445f04350ca90",
+    "url": "/react-shopping-game/index.html"
+  }
+];
