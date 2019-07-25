8a6421d4ed3737c0229acce4243ba6090ab03b38
brandongatlin <brandongatlin1981@me.com>
2019-07-24 21:06:21 -0500

    added header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
 
+import Header from "./components/header";
+
 import ClickBox from "./components/containers/click_box";
 
 const App = () => {
   return (
     <div className='App'>
+      <Header />
       <ClickBox />
     </div>
   );
---
+++ src/components/header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = props => {
+  return (
+    <>
+      <h1>Budget Game:</h1>
+      <p>
+        Buys as many items as you like, but you must spend all your money
+        without going over bugget!
+      </p>
+    </>
+  );
+};
+
+export default Header;
